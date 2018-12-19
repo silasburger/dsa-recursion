44f6fdcfc4e4321d7ee65f808d4abb092e4a287d
Silas Burger <ybsilas@gmail.com>
2018-12-18 16:29:13 -0800

    finished palindrome; starting findIndex

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,3 +1,5 @@
-function isPalindrome(string) {
+function isPalindrome(string, i = 0) {
+  if(i===Math.floor(string.length/2)) return true;
+  return string[i] === string[string.length-1-i] && isPalindrome(string, i+1);
   
 }
