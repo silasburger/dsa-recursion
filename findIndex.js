fed2667afc6e91cce9c7e9b01914abd4e081ef4f
Silas Burger <ybsilas@gmail.com>
2018-12-18 20:38:13 -0800

    started gather strings

--- findIndex.js
+++ findIndex.js
@@ -1,4 +1,5 @@
-function findIndex() {
-
-  
+function findIndex(arr, value, i=0) {
+  if(arr[i]===value) return i;
+  if(i===arr.length) return -1;
+  return findIndex(arr, value, i=i+1);
 }
---
+++ gatherStrings.js
@@ -0,0 +1,18 @@
+function gatherStrings() {
+  //base case is when there is no more objects in values
+  //use loop for progress
+}
+
+let nestedObj = {
+  firstName: "Lester",
+  favoriteNumber: 22,
+  moreData: {
+    lastName: "Testowitz"
+  },
+  funFacts: {
+    moreStuff: {
+      anotherNumber: 100
+    },
+    favoriteString: "nice!"
+  }
+}
